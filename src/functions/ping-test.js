80f4464ca01ac37d21116087ab3277f57aa7015b
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-06 20:47:10 -0500

    Added content for ping-test

--- src/functions/ping-test.js
+++ src/functions/ping-test.js
@@ -4,9 +4,12 @@ app.http('ping-test', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
 
         const name = request.query.get('name') || await request.text() || 'world';
+        const type = request.json['type'];
+        if(type == 1){
+            return JSON.stringify({"type" : 1});
+        }
 
         return { body: `Hello, ${name}!` };
     }
