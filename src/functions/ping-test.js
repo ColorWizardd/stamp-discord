4b077b5dc565b515364d22d6cda0539ff8f8bacd
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-06 20:31:04 -0500

    Fixed ping-test

--- src/functions/ping-test.js
+++ src/functions/ping-test.js
@@ -1,7 +1,7 @@
 const { app } = require('@azure/functions');
 
 app.http('ping-test', {
-    methods: ['POST'],
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
