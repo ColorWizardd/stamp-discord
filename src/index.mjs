158d5ba1e732058ac1497e9d6079ea8b8c43d6d6
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-06 23:08:35 -0500

    Switched to mjs, fixed import syntax

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
-import { app } from ('@azure/functions');
-import { SlashCreator, AzureFunctionV4Server } from ('slash-create');
+import { app } from '@azure/functions';
+import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
+import path from 'path';
 const env = process.env;
 
 const creator = new SlashCreator({
@@ -8,5 +9,5 @@ const creator = new SlashCreator({
     token: env.BOT_TOKEN
 });
 creator.withServer(new AzureFunctionV4Server(app));
-await creator.registerCommandsIn(__dirname, "commands");
+await creator.registerCommandsIn(path.join(__dirname, "commands"));
 await creator.syncCommands();
