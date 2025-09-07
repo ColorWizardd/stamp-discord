987036cd728a9dc67ffd3d7ab7ab4c7f38beb0c2
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-07 13:05:14 -0500

    Added http functionality to index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import { app } from '@azure/functions';
 import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
 import path from 'path';
 
-const creator = new SlashCreator({
-    applicationID : process.env.APPLICATION_ID,
-    publicKey : process.env.PUBLIC_KEY,
-    token: process.env.BOT_TOKEN
+app.http("index", {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        const req = JSON.parse(request);
+        if(req.status == "1"){
+            return JSON.stringify({
+                status: "200",
+                type: "1"
+            });
+        }
+        const creator = new SlashCreator({
+            applicationID : process.env.APPLICATION_ID,
+            publicKey : process.env.PUBLIC_KEY,
+            token: process.env.BOT_TOKEN
+        });
+        creator.withServer(new AzureFunctionV4Server(app));
+        await creator.registerCommandsIn(path.join(__dirname, "commands"));
+        await creator.syncCommands();
+    }
 });
-creator.withServer(new AzureFunctionV4Server(app));
-await creator.registerCommandsIn(path.join(__dirname, "commands"));
-await creator.syncCommands();
+
