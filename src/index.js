a442599fc94b42ef76768754a2216b7056e0097b
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-07 18:31:13 -0500

    Changed packages/imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+import * as appInsights from 'applicationinsights';
 import { app } from '@azure/functions';
 import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
-import path from 'node:path';
-import { loadEnvFile } from 'process';
+import { join } from 'node:path';
 
-loadEnvFile("./.env")
+process.loadEnvFile("./.env")
+appInsights.setup(process.env.APPINSIGHTS_CONNECTION_STRING);
 
-async function init() {
-    const creator = new SlashCreator({
-        applicationID : process.env.APPLICATION_ID,
-        publicKey : process.env.PUBLIC_KEY,
-        token: process.env.BOT_TOKEN
-    });
-    creator.withServer(new AzureFunctionV4Server(app));
-    await creator.registerCommandsIn(path.join(__dirname, "commands"));
-    await creator.syncCommands();
-}
-
-await init();
+const creator = new SlashCreator({
+    applicationID : process.env.APPLICATION_ID,
+    publicKey : process.env.PUBLIC_KEY,
+    token: process.env.BOT_TOKEN
+});
+creator.withServer(new AzureFunctionV4Server(app));
+await creator.registerCommandsIn(join(import.meta.dirname, "commands"));
+await creator.syncCommands();
