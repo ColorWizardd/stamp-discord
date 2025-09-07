79dacfb67cb6ee20e175131e83449b686a21eb89
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-07 17:59:11 -0500

    Testing new index format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { app } from '@azure/functions';
 import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
-import path from 'path';
+import path from 'node:path';
+import { loadEnvFile } from 'process';
 
-const creator = new SlashCreator({
-    applicationID : process.env.APPLICATION_ID,
-    publicKey : process.env.PUBLIC_KEY,
-    token: process.env.BOT_TOKEN
-});
-creator.withServer(new AzureFunctionV4Server(app));
-await creator.registerCommandsIn(path.join(__dirname, "commands"));
-await creator.syncCommands();
+loadEnvFile("./.env")
+
+async function init() {
+    const creator = new SlashCreator({
+        applicationID : process.env.APPLICATION_ID,
+        publicKey : process.env.PUBLIC_KEY,
+        token: process.env.BOT_TOKEN
+    });
+    creator.withServer(new AzureFunctionV4Server(app));
+    await creator.registerCommandsIn(path.join(__dirname, "commands"));
+    await creator.syncCommands();
+}
+
+await init();
