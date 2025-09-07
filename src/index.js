d92ee876b4738d25d9322f85947b2587f13b4634
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-07 00:17:05 -0500

    Updated slashcreator to use env directly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { app } from '@azure/functions';
 import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
 import path from 'path';
-const env = process.env;
 
 const creator = new SlashCreator({
-    applicationID : env.APPLICATION_ID,
-    publicKey : env.PUBLIC_KEY,
-    token: env.BOT_TOKEN
+    applicationID : process.env.APPLICATION_ID,
+    publicKey : process.env.PUBLIC_KEY,
+    token: process.env.BOT_TOKEN
 });
 creator.withServer(new AzureFunctionV4Server(app));
 await creator.registerCommandsIn(path.join(__dirname, "commands"));
