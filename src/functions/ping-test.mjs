4e7922f806a9779e8b8a38a4465e75ef38bcf999
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-06 23:48:21 -0500

    Removed function.json (wasn't needed)

--- src/functions/ping-test.mjs
+++ src/functions/ping-test.mjs
@@ -5,7 +5,6 @@ app.http('ping-test', {
     authLevel: 'anonymous',
     handler: async (request, context) => {
 
-        const name = request.query.get('name') || await request.text() || 'world';
         const type = request.json['type'];
         if(type == 1){
             return JSON.stringify({
@@ -13,7 +12,9 @@ app.http('ping-test', {
                 type : 1
             });
         }
+        return JSON.stringify({
+            status : 400
+        });
+        }
 
-        return { body: `Hello, ${name}!` };
-    }
 });
