58a339b06bb84437a71a8dfaea710ecde89455df
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-11 11:08:11 -0500

    Added http binding to index

--- src/index.js
+++ src/index.js
@@ -4,24 +4,30 @@ import { SlashCreator, AzureFunctionV4Server } from 'slash-create';
 import { join } from 'node:path';
 import { PingCommand } from './commands/PingCommand'
 
+process.loadEnvFile("./.env")
 appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
 
 app.setup({
     enableHttpStream: true
 });
 
-process.loadEnvFile("./.env")
-
-const creator = new SlashCreator({
-    applicationID : process.env.APPLICATION_ID,
-    publicKey : process.env.PUBLIC_KEY,
-    token: process.env.BOT_TOKEN
-});
-creator.withServer(new AzureFunctionV4Server(app));
-creator.registerCommands(
-    [
-        PingCommand
-    ],
-    false
-);
-await creator.syncCommands();
+app.http("index", {
+    methods: ["POST"],
+    authLevel: "anonymous",
+    handler: async () => {
+        
+        const creator = new SlashCreator({
+            applicationID : process.env.APPLICATION_ID,
+            publicKey : process.env.PUBLIC_KEY,
+            token: process.env.BOT_TOKEN
+        });
+        creator.withServer(new AzureFunctionV4Server(app));
+        creator.registerCommands(
+            [
+                PingCommand
+            ],
+            false
+        );
+        await creator.syncCommands();
+    }
+} )
