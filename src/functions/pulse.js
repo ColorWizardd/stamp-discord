81a6ecb9a88987ab2c65ca587281335bbfe497d4
ColorWizardd <colorwizardd.biz@gmail.com>
2025-09-23 16:30:31 -0500

    Added warm-up functions

---
+++ src/functions/pulse.js
@@ -0,0 +1,11 @@
+import { app, HttpRequest, HttpResponse } from '@azure/functions';
+
+app.timer('pulse', {
+    schedule: '0,30 6-22 * * *',
+    handler: (myTimer, context) => {
+        context.log('Sending pulse to server...');
+        async () => {
+            await fetch("https://stamp-discord.azurewebsites.net/api/ping");
+        }
+    }
+});
